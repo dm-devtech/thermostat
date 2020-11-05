2e4e5f03ce5b70db60a70841d5727378fea7aa3c
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-05 15:25:27 +0000

    Added business logic all tests pass. This includes reset function, usage function and power saving function.

--- jasmine/spec/thermostatSpec.js
+++ jasmine/spec/thermostatSpec.js
@@ -21,14 +21,70 @@ describe('Thermostat', function() {
 
   it('checks that temperature is never below 10', function(){
     thermostat.temperature = 10
-    expect(function() {thermostat.decreaseTemp()}).toThrowError('Minimum temp is 10')
+    thermostat.decreaseTemp()
+    expect(thermostat.temperature).toEqual(10)
   });
 
   it('checks when power saving mode is on, the temp cannot be increased beyond 25 degrees', function(){
     thermostat.temperature = 25
     thermostat.powerSaving('on')
-    expect(function() {thermostat.increaseTemp()}).toThrowError('The temp cannot be increased due to PS mode ON')
+    thermostat.increaseTemp()
+    expect(thermostat.temperature).toEqual(25)
   });
+
+  it('checks when power saving mode is off, the temp cannot be increased beyond 32 degrees', function(){
+    thermostat.temperature = 32
+    thermostat.powerSaving('off')
+    thermostat.increaseTemp()
+    expect(thermostat.temperature).toEqual(32)
+  });
+
+  it('checks that powersaving is on by default', function() {
+    expect(thermostat.powersaving).toEqual(true)
+  });
+
+  it('checks that reset function sets temp to 20', function() {
+    thermostat.increaseTemp()
+    thermostat.reset()
+    expect(thermostat.temperature).toEqual(20)
+  });
+
+  it('checks that energyUsage function returns low usage when temp less than 18 degress', function() {
+    thermostat.decreaseTemp()
+    thermostat.decreaseTemp()
+    thermostat.decreaseTemp()
+    expect(thermostat.energyUsage()).toEqual('low-usage')
+  });
+
+  it('checks that energyUsage function returns medium usage when temp > 18 and <= 25 degress', function() {
+    thermostat.temperature = 25
+    expect(thermostat.energyUsage()).toEqual('medium-usage')
+  });
+
+  it('checks that energyUsage function returns medium usage when temp > 25 degress', function() {
+    thermostat.temperature = 26
+    expect(thermostat.energyUsage()).toEqual('high-usage')
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -1,21 +1,37 @@
 class Thermostat{
 
   constructor(){
-    this.temperature = 20
-    this.powersaving = false
+    this.DEFAULTTEMP = 20
+    this.MINIMUMTEMP = 10
+    this.PSONMAXTEMP = 25
+    this.PSOFFMAXTEMP = 32
+    this.LOWUSAGE = 18
+    this.MEDUSAGE = 25
+    this.temperature = this.DEFAULTTEMP
+    this.powersaving = true
   };
 
   increaseTemp() {
-    if(this.powersaving === true && this.temperature === 25 ) {
-      throw new Error('The temp cannot be increased due to PS mode ON')
+    if(this.isPowerSavingOn()) {
+      return;
+    } else if (this.isPowerSavingOff()) {
+      return;
     } else {
       this.temperature ++
     }
   };
 
+  isPowerSavingOn() {
+    return this.powersaving === true && this.temperature === this.PSONMAXTEMP;
+  }
+
+  isPowerSavingOff() {
+    return this.powersaving === false && this.temperature === this.PSOFFMAXTEMP;
+  }
+
   decreaseTemp() {
-    if (this.temperature === 10) {
-      throw new Error('Minimum temp is 10')
+    if (this.temperature === this.MINIMUMTEMP ) {
+      return;
   } else {
       this.temperature --
     }
@@ -28,8 +44,20 @@ class Thermostat{
     else if (tempSetting === 'off') {
       this.powersaving = false
     }
-
   };
 
+  reset() {
+    this.temperature = this.DEFAULTTEMP
+  }
+
+  energyUsage() {
+    if(this.temperature < this.LOWUSAGE) {
+      return 'low-usage';
+    } else if (this.temperature <= this.MEDUSAGE ) {
+      return 'medium-usage'
+    } else if (this.temperature > this.MEDUSAGE ) {
+      return 'high-usage'
+    }
+  };
 
 };
